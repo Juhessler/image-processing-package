4404272a358f400eb35666837136d84dd800bb2e
Juliana Hessler <juehessler@gmail.com>
2024-10-30 14:01:21 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description="Test version - Image processing. This project belongs to Karina Tiemi Kato, Tech Lead, Machine Learning Engineer, Data Scientist Specialist at Take. This package is a demo for simulation of upload on the Test Pypi website, and it's from class of the Bootcamp developer full stack Python. E-mail:karinatkato@gmail.com.",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="https://https://github.com/Juhessler/image-processing-package.git"
+    url="https://github.com/Juhessler/image-processing-package.git"
     install_requires=requirements,
     python_requires='>=3.8',
 )
